5787ea00678db7dbf7184b1c1fbe74281af55956
2waybene <jianying.li@gmail.com>
2013-10-29 16:42:59 -0400

    modified files

--- Scripts/NB_summaryDT.R
+++ Scripts/NB_summaryDT.R
@@ -32,9 +32,72 @@ boxplot(dt4model[-1])
 title("Box plot on pilot data")
 mtext("Missing was imputed with median or mean")
 str(dt4model)
+setwd("X:/myGit/mixturemodel/firstDraft")
+save(dt4model, file ="pilot_data_noMissing.Rda")
 ##=====================================================================
 
+##=============================================================
+##	Split datat into training and testing
+##=============================================================
+dt4model [,1] <- as.factor(c( rep ("norm", 9), rep("olk", 9), rep ("oscc", 9)))
+
+dt4model2 <- dt4model[c(1:6)]
+
+
+set.seed(1234)
+testID <-c ()
+for (i in 1:3)
+{
+	testID[c((i*2-1), i*2)] <-  (i-1)*9 + (sample(c(1:9),2))
+}
+testID 	
+
+
+trainDT <- dt4model2[-testID,]
+testDT  <- dt4model2[testID,]
+
+str(trainDT)
+str(testDT)
+
+ans <- testDT[,1]
+##=================================================
+#	Try some prediction models
+##=================================================
+#	Naive Bayes
+##=================================================
+library(e1071)
+trainNB =  trainDT 
+testNB  =  testDT
+modelNB2 <- naiveBayes(Group ~ ., data = trainNB)
+predNB2 <- predict(modelNB2, testNB[,-1])
+predNB2
+table(predNB2, ans)
+
+##=================================================
+##	svm in e1071 package
+##=================================================
+trainSVM =  trainDT 
+testSVM  =  testDT
+modelSVM <- svm (Group ~ ., data = trainSVM)
+predSVM  <- predict (modelSVM, testSVM[,-1])
+predSVM
+
+ans <- testSVM[,1]
+table(predSVM, ans)
+
+
+##=======================================================================
 library(caret)
+
+
+# train LDA model
+ldaFit <- train( x = trainDT[,-1], y = trainDT[,1],  method = "lda")
+predLDA <- predict (ldaFit, testDT[,-1])
+predLDA
+
+
+##======================================================================
+
 TrainData <- dt4model[,-1]
 TrainClasses <- dt4model[,1]
 trt=trainControl(method='cv',number=10)
@@ -42,13 +105,7 @@ nbFit <- train(TrainData, TrainClasses, method = "nb", trControl = trt)
 
 
 
-##=========================================================================
-library(e1071)
-trainNB =  dt4model
 
-modelNB2 <- naiveBayes(Group ~ ., data = trainNB)
-predNB2 <- predict(modelNB2, trainNB)
-predNB2
 
 
 
@@ -58,34 +115,6 @@ nbFit2 <- NaiveBayes(TrainData, TrainClasses)
 , usekernel=TRUE)
 
 
-
-##=============================================================
-##	Split datat into training and testing
-##=============================================================
-
-# prepare data for caret training: separated labels and features
-Ygtm <- dt4model[1] # convert sub data frame to vector
-Xcur <- dt4model[-1] # get features without genotype labels
-
-
-
-
-# split train and test sets (half/half)
-library(caret)
-set.seed(1)
-testID <-c ()
-for (i in 1:3)
-{
-	testID[c((i*2-1), i*2)] <-  (i-1)*9 + (sample(c(1:9),2))
-}
-testID 	
-
-
-trainX <- Xcur[-testID,]
-testX <- Xcur[testID,]
-
-trainY <- Ygtm[-testID,]
-testY <- Ygtm[testID,]
 
 
 
