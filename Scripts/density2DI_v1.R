d082b190ffbdefd14e1cc5da2a23cf0a1f3e4b09
2waybene <jianying.li@gmail.com>
2014-03-05 15:02:49 -0500

    as of 03-05-2014

--- Scripts/density2DI_v1.R
+++ Scripts/density2DI_v1.R
@@ -44,26 +44,35 @@ den1 <- density(den1.dt)
 den2 <- density(den2.dt)
 
 
-y = den1$y + den2$y
-
-##	D.I. values
-
+y = c(den1.dt,den2.dt)
+plot(density(y))
 
 
-#Sample data
-dat <- data.frame(dens = c(rnorm(100), rnorm(100, 10, 5))
-                   , lines = rep(c("a", "b"), each = 100))
+#Sample data with unbalanced proportion
+
+dat.2 <- data.frame(dens = c(rnorm(1000), rnorm(10, 10, 5))  , lines = c(rep("a",1000), rep( "b", 10)))
+densityplot(~dens, data=dat.2, groups = lines,  plot.points = FALSE, ref = TRUE, auto.key = list(space = "right"))
+
+plot(density(dat.2$dens), main = "Density of mixture of two data sets")
+plot(density(dat.2$dens[which(dat.2$lines=="a")]), main = "Density of 1000 norm sim data")
+plot(density(dat.2$dens[-which(dat.2$lines=="a")]), main = "Density of 10 norm (10,5) sim data")
+
+
 #Plot.
 
-windows()
-plot(density(dat$dens, group = dat$lines))
 
-str(dat)
+library(ggplot2)
+ggplot(dat.2, aes(x = dens, fill = lines)) + geom_density(alpha = 0.5)
 
-dat$dens
-summary(dat$lines)
+library(lattice)
+densityplot(~dens,data=dat.2, groups = lines, plot.points = FALSE, ref = TRUE, auto.key = list(space = "right"))
+
+##========================================================
+#	On a real D.I. value data
+##=========================================================
 
 
-
-library(affy)
-plotDensity(dat$dens)
+f_IN <-  "x:/myGit/mixturemodel/data/128110.csv"
+dt <- read.csv (f_IN)
+title = paste ("sample_", sub(".csv", "", f), sep="")
+plot(density(as.vector(dt$DNA_Index)), ylab = "Density", xlab = "DNA Index value", main= title )
